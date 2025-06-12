7386fd28a8e453568a0990c40feab093de03bf8b
tharumin <94857655+tharumin@users.noreply.github.com>
2025-06-12 15:24:09 +0530

    Add files via upload

--- pair.js
+++ pair.js
@@ -73,7 +73,7 @@ var randomItem = selectRandomItem(items);
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
-                        let md = "DARK-SILENCE-MD~" + string_session;
+                        let md = string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
                         let desc = `
 *┏━━━━━━━━━━━━━━*
