67485e7a1932c1f9ac7100872f5c7455f5a67c16
tharumin <94857655+tharumin@users.noreply.github.com>
2026-02-24 18:07:49 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -73,28 +73,37 @@ var randomItem = selectRandomItem(items);
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
-                        let md = string_session;
+                        let md = "𝕃𝔸ℝ𝔸-𝕄𝔻="+string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
                         let desc = `
-*┏━━━━━━━━━━━━━━*
-*┃LARA-MD SESSION IS*
-*┃SUCCESSFULLY*
-*┃CONNECTED ✅🔥*
-*┗━━━━━━━━━━━━━━━*
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-*❶ || Creator = SADEESHA CODER*
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-*❷ || Owner =* https://wa.me/+94779062397
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-*POWERED BY SADEESHA CODER*
+✾━┫ *⚬Lααɾα-ꜱᴇꜱꜱɪᴏɴ⚬* ┣━✾
+                   *ᴸ  ͣ  ͣ  ͬ  ͣ  ✻  ᴸ  ͣ  ͣ  ͬ  ͣ*
+
+*𝙻𝚊𝚛𝚊 𝙼𝙳 𝚜𝚎𝚜𝚜𝚒𝚘𝚗 𝚒𝚍 𝚜𝚞𝚌𝚌𝚎𝚜𝚜𝚏𝚞𝚕𝚕𝚢 𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚎𝚍 ✅*
+
+*𝗔𝗧𝗧𝗘𝗡𝗧𝗜𝗢𝗡* ⚠️
+
+_𝚃𝚑𝚒𝚜 𝚒𝚜 𝚢𝚘𝚞𝚛 𝚠𝚑𝚊𝚝𝚜𝚊𝚙𝚙 𝚕𝚘𝚐𝚒𝚗𝚐 𝚌𝚘𝚍𝚎. 𝙳𝚘𝚗'𝚝 𝚜𝚑𝚎𝚊𝚛 𝚊𝚗𝚢𝚘𝚗𝚎 ❌_
+
+𝗢𝗪𝗡𝗘𝗥 🛠️
+
+𝙵𝚊𝚌𝚎𝚋𝚘𝚘𝚔 :
+https://www.facebook.com/share/1CWyrVEcK9/
+
+𝙸𝚗𝚜𝚝𝚊𝚐𝚛𝚊𝚖 :
+https://www.instagram.com/sadeesha_min?igsh=bWRlem5jMTA5OHo4
+
+𝚈𝚘𝚞𝚃𝚞𝚋𝚎 :
+https://www.youtube.com/channel/UC7473CyG_w74rHZl-uQA64g
+
+> *ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴇᴇꜱʜᴀ ᴄᴏᴅᴇʀ*
 `; 
                         await sock.sendMessage(sock.user.id, {
 text: desc,
 contextInfo: {
 externalAdReply: {
-title: "SADEESHA CODER👨🏻‍💻",
-thumbnailUrl: "https://github.com/manpakaya/Lara_Data_Base/raw/refs/heads/main/20250224_162020.jpg",
+title: "𝙎𝘼𝘿𝙀𝙀𝙎𝙃𝘼 𝘾𝙊𝘿𝙀𝙍 👨🏻‍💻",
+thumbnailUrl: "https://raw.githubusercontent.com/tharumin/Alexa_Voice/refs/heads/main/STK-20260116-WA0002.png",
 sourceUrl: "https://whatsapp.com/channel/0029VaD5t8S1nozDfDDjRj2J",
 mediaType: 1,
 renderLargerThumbnail: false
@@ -107,24 +116,33 @@ sock.groupAcceptInvite('Ci5mDk9zEVF95NcuqEtzl4')
                     } catch (e) {
                             let ddd = sock.sendMessage(sock.user.id, { text: e });
                             let desc = `
-*┏━━━━━━━━━━━━━━*
-*┃LARA-MD SESSION IS*
-*┃SUCCESSFULLY*
-*┃CONNECTED ✅🔥*
-*┗━━━━━━━━━━━━━━━*
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-*❶ || Creator = SADEESHA CODER*
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-*❷ || Owner =* https://wa.me/+94779062397
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-*POWERED BY SADEESHA CODER*`;
+ ✾━┫ *⚬Lααɾα-ꜱᴇꜱꜱɪᴏɴ⚬* ┣━✾
+                   *ᴸ  ͣ  ͣ  ͬ  ͣ  ✻  ᴸ  ͣ  ͣ  ͬ  ͣ*
+
+*𝙻𝚊𝚛𝚊 𝙼𝙳 𝚜𝚎𝚜𝚜𝚒𝚘𝚗 𝚒𝚍 𝚜𝚞𝚌𝚌𝚎𝚜𝚜𝚏𝚞𝚕𝚕𝚢 𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚎𝚍 ✅*
+
+*𝗔𝗧𝗧𝗘𝗡𝗧𝗜𝗢𝗡* ⚠️
+
+_𝚃𝚑𝚒𝚜 𝚒𝚜 𝚢𝚘𝚞𝚛 𝚠𝚑𝚊𝚝𝚜𝚊𝚙𝚙 𝚕𝚘𝚐𝚒𝚗𝚐 𝚌𝚘𝚍𝚎. 𝙳𝚘𝚗'𝚝 𝚜𝚑𝚎𝚊𝚛 𝚊𝚗𝚢𝚘𝚗𝚎 ❌_
+
+𝗢𝗪𝗡𝗘𝗥 🛠️
+
+𝙵𝚊𝚌𝚎𝚋𝚘𝚘𝚔 :
+https://www.facebook.com/share/1CWyrVEcK9/
+
+𝙸𝚗𝚜𝚝𝚊𝚐𝚛𝚊𝚖 :
+https://www.instagram.com/sadeesha_min?igsh=bWRlem5jMTA5OHo4
+
+𝚈𝚘𝚞𝚃𝚞𝚋𝚎 :
+https://www.youtube.com/channel/UC7473CyG_w74rHZl-uQA64g
+
+> *ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴇᴇꜱʜᴀ ᴄᴏᴅᴇʀ*`;
                             await sock.sendMessage(sock.user.id, {
 text: desc,
 contextInfo: {
 externalAdReply: {
-title: "SADEESHA CODER👨🏻‍💻",
-thumbnailUrl: "https://github.com/manpakaya/Lara_Data_Base/raw/refs/heads/main/20250224_162020.jpg",
+title: "𝙎𝘼𝘿𝙀𝙀𝙎𝙃𝘼 𝘾𝙊𝘿𝙀𝙍 👨🏻‍💻",
+thumbnailUrl: "https://raw.githubusercontent.com/tharumin/Alexa_Voice/refs/heads/main/STK-20260116-WA0002.png",
 sourceUrl: "https://whatsapp.com/channel/0029VaD5t8S1nozDfDDjRj2J",
 mediaType: 2,
 renderLargerThumbnail: false,
